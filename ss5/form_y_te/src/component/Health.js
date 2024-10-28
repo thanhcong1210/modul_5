67e4340f1abc2cf148059ded47edd6ad23ba9ad9
thanhcong1210 <danglethanhcong4@gmail.com>
2024-10-28 10:28:09 +0700

    nop bai tap

--- ss5/form_y_te/src/component/Health.js
+++ ss5/form_y_te/src/component/Health.js
@@ -75,115 +75,118 @@ export default function Health() {
                 onSubmit={saveHealth}
                 validationSchema={Yup.object(objectValid)}
             >
-                <Form className="border p-4 rounded shadow">
-                    <h2 className="text-center mb-4">Tờ khai y tế</h2>
+                <Form className="border p-5 rounded shadow bg-white">
+                    <h2 className="text-center mb-5">Tờ khai y tế</h2>
 
-                    <div className="mb-3">
-                        <label className="form-label">Họ tên</label>
-                        <Field name="name" className="form-control"/>
-                        <ErrorMessage name="name" component="div" className="text-danger"/>
-                    </div>
-
-                    <div className="mb-3">
-                        <label className="form-label">Số hộ chiếu/CMND</label>
-                        <Field name="cmnd" className="form-control"/>
-                        <ErrorMessage name="cmnd" component="div" className="text-danger"/>
-                    </div>
-
-                    <div className="mb-3">
-                        <label className="form-label">Năm sinh</label>
-                        <Field name="dob" type="number" className="form-control"/>
-                        <ErrorMessage name="dob" component="div" className="text-danger"/>
-                    </div>
-
-                    <div className="mb-3">
-                        <label className="form-label">Giới tính</label>
-                        <div>
-                            {SEX_LIST.map((sex) => (
-                                <div key={sex.value} className="form-check form-check-inline">
-                                    <Field
-                                        type="radio"
-                                        name="gender"
-                                        value={sex.value}
-                                        className="form-check-input"
-                                    />
-                                    <label className="form-check-label">{sex.label}</label>
-                                </div>
-                            ))}
+                    <div className="row mb-4">
+                        <div className="col-md-6">
+                            <label className="form-label">Họ tên</label>
+                            <Field name="name" className="form-control" />
+                            <ErrorMessage name="name" component="div" className="text-danger mt-1" />
                         </div>
-                        <ErrorMessage name="gender" component="div" className="text-danger"/>
+                        <div className="col-md-6">
+                            <label className="form-label">Số hộ chiếu/CMND</label>
+                            <Field name="cmnd" className="form-control" />
+                            <ErrorMessage name="cmnd" component="div" className="text-danger mt-1" />
+                        </div>
                     </div>
 
-                    <div className="mb-3">
-                        <label className="form-label">Quốc tịch</label>
-                        <Field name="country" className="form-control"/>
-                        <ErrorMessage name="country" component="div" className="text-danger"/>
+                    <div className="row mb-4">
+                        <div className="col-md-6">
+                            <label className="form-label">Năm sinh</label>
+                            <Field name="dob" type="number" className="form-control" />
+                            <ErrorMessage name="dob" component="div" className="text-danger mt-1" />
+                        </div>
+                        <div className="col-md-6">
+                            <label className="form-label">Giới tính</label>
+                            <div className="d-flex gap-3">
+                                {SEX_LIST.map((sex) => (
+                                    <div key={sex.value} className="form-check">
+                                        <Field
+                                            type="radio"
+                                            name="gender"
+                                            value={sex.value}
+                                            className="form-check-input"
+                                        />
+                                        <label className="form-check-label ms-2">{sex.label}</label>
+                                    </div>
+                                ))}
+                            </div>
+                            <ErrorMessage name="gender" component="div" className="text-danger mt-1" />
+                        </div>
                     </div>
 
-                    <div className="mb-3">
-                        <label className="form-label">Công ty làm việc</label>
-                        <Field name="company" className="form-control"/>
-                        <ErrorMessage name="company" component="div" className="text-danger"/>
+                    <div className="row mb-4">
+                        <div className="col-md-6">
+                            <label className="form-label">Quốc tịch</label>
+                            <Field name="country" className="form-control" />
+                            <ErrorMessage name="country" component="div" className="text-danger mt-1" />
+                        </div>
+                        <div className="col-md-6">
+                            <label className="form-label">Công ty làm việc</label>
+                            <Field name="company" className="form-control" />
+                            <ErrorMessage name="company" component="div" className="text-danger mt-1" />
+                        </div>
                     </div>
 
-                    <div className="mb-3">
-                        <label className="form-label">Bộ phận làm việc</label>
-                        <Field name="working" className="form-control"/>
-                        <ErrorMessage name="working" component="div" className="text-danger"/>
+                    <div className="row mb-4">
+                        <div className="col-md-6">
+                            <label className="form-label">Bộ phận làm việc</label>
+                            <Field name="working" className="form-control" />
+                            <ErrorMessage name="working" component="div" className="text-danger mt-1" />
+                        </div>
+                        <div className="col-md-6 form-check">
+                            <Field name="insurance" type="checkbox" className="form-check-input" />
+                            <label className="form-check-label ms-2">Có thẻ bảo hiểm y tế</label>
+                            <ErrorMessage name="insurance" component="div" className="text-danger mt-1" />
+                        </div>
                     </div>
 
-                    <div className="mb-3 form-check">
-                        <label className="form-check-label">Có thẻ bảo hiểm y tế</label>
-                        <Field name="insurance" type="checkbox" className="form-check-input"/>
-                        <ErrorMessage name="insurance" component="div" className="text-danger"/>
+                    <h4 className="mt-5 mb-4">Địa chỉ liên lạc tại Việt Nam</h4>
+                    <div className="row mb-4">
+                        <div className="col-md-4">
+                            <label className="form-label">Tỉnh thành</label>
+                            <Field name="city" className="form-control" />
+                            <ErrorMessage name="city" component="div" className="text-danger mt-1" />
+                        </div>
+                        <div className="col-md-4">
+                            <label className="form-label">Quận / huyện</label>
+                            <Field name="district" className="form-control" />
+                            <ErrorMessage name="district" component="div" className="text-danger mt-1" />
+                        </div>
+                        <div className="col-md-4">
+                            <label className="form-label">Phường / xã</label>
+                            <Field name="ward" className="form-control" />
+                            <ErrorMessage name="ward" component="div" className="text-danger mt-1" />
+                        </div>
                     </div>
 
-                    <h3 className="mb-4">Địa chỉ liên lạc tại Việt Nam</h3>
-
-                    <div className="mb-3">
-                        <label className="form-label">Tỉnh thành</label>
-                        <Field name="city" className="form-control"/>
-                        <ErrorMessage name="city" component="div" className="text-danger"/>
-                    </div>
-
-                    <div className="mb-3">
-                        <label className="form-label">Quận / huyện</label>
-                        <Field name="district" className="form-control"/>
-                        <ErrorMessage name="district" component="div" className="text-danger"/>
-                    </div>
-
-                    <div className="mb-3">
-                        <label className="form-label">Phường / xã</label>
-                        <Field name="ward" className="form-control"/>
-                        <ErrorMessage name="ward" component="div" className="text-danger"/>
-                    </div>
-
-                    <div className="mb-3">
+                    <div className="mb-4">
                         <label className="form-label">Số nhà, phố, tổ dân phố / thôn / đội</label>
-                        <Field name="house" className="form-control"/>
-                        <ErrorMessage name="house" component="div" className="text-danger"/>
+                        <Field name="house" className="form-control" />
+                        <ErrorMessage name="house" component="div" className="text-danger mt-1" />
                     </div>
 
-                    <div className="mb-3">
-                        <label className="form-label">Điện thoại</label>
-                        <Field name="phone" className="form-control"/>
-                        <ErrorMessage name="phone" component="div" className="text-danger"/>
+                    <div className="row mb-4">
+                        <div className="col-md-6">
+                            <label className="form-label">Điện thoại</label>
+                            <Field name="phone" className="form-control" />
+                            <ErrorMessage name="phone" component="div" className="text-danger mt-1" />
+                        </div>
+                        <div className="col-md-6">
+                            <label className="form-label">Email</label>
+                            <Field name="email" className="form-control" />
+                            <ErrorMessage name="email" component="div" className="text-danger mt-1" />
+                        </div>
                     </div>
 
-                    <div className="mb-3">
-                        <label className="form-label">Email</label>
-                        <Field name="email" className="form-control"/>
-                        <ErrorMessage name="email" component="div" className="text-danger"/>
+                    <div className="mb-4">
+                        <h5>Quốc gia / vùng lãnh thổ đã đến trong 14 ngày qua</h5>
+                        <Field name="descrip" as="textarea" className="form-control" />
                     </div>
 
-                    <div className="mb-3">
-                        <h5>Trong vòng 14 ngày qua, Anh/chị có đến quốc gia / vùng lãnh thổ nào (Có thể đi qua nhiều
-                            quốc gia)</h5>
-                        <Field name="descrip" as="textarea" className="form-control"/>
-                    </div>
-
-                    <div className="mb-3">
-                        <h5>Trong vòng 14 ngày qua, anh/chị có thấy xuất hiện dấu hiệu nào sau đây không</h5>
+                    <div className="mb-4">
+                        <h5>Các dấu hiệu xuất hiện trong 14 ngày qua</h5>
                         {TOKEN.map((item) => (
                             <div key={item.value} className="form-check">
                                 <Field
@@ -192,13 +195,13 @@ export default function Health() {
                                     value={item.value}
                                     className="form-check-input"
                                 />
-                                <label className="form-check-label">{item.label}</label>
+                                <label className="form-check-label ms-2">{item.label}</label>
                             </div>
                         ))}
                     </div>
 
-                    <div className="mb-3">
-                        <h5>Trong vòng 14 ngày qua, Anh/chị có tiếp xúc với</h5>
+                    <div className="mb-5">
+                        <h5>Tiếp xúc trong 14 ngày qua</h5>
                         {CONTACT.map((item) => (
                             <div key={item.value} className="form-check">
                                 <Field
@@ -207,39 +210,37 @@ export default function Health() {
                                     value={item.value}
                                     className="form-check-input"
                                 />
-                                <label className="form-check-label">{item.label}</label>
+                                <label className="form-check-label ms-2">{item.label}</label>
                             </div>
                         ))}
                     </div>
 
-                    <button type="submit" className="btn btn-primary">Submit</button>
+                    <button type="submit" className="btn btn-primary w-100">Submit</button>
                 </Form>
             </Formik>
 
             {form && (
-                <div className="mt-5 p-4 border rounded bg-light">
-                    <h2>Thông tin đã lưu</h2>
-                    <p><strong>Họ tên:</strong> {form.name}</p>
-                    <p><strong>Số hộ chiếu/CMND:</strong> {form.cmnd}</p>
-                    <p><strong>Năm sinh:</strong> {form.dob}</p>
-                    <p><strong>Giới tính:</strong> {form.gender}</p>
-                    <p><strong>Quốc tịch:</strong> {form.country}</p>
-                    <p><strong>Công ty làm việc:</strong> {form.company}</p>
-                    <p><strong>Bộ phận làm việc:</strong> {form.working}</p>
-                    <p><strong>Có thẻ bảo hiểm y tế:</strong> {form.insurance ? "Có" : "Không"}</p>
-                    <p><strong>Địa chỉ liên lạc tại Việt Nam:</strong></p>
-                    <p><strong>Tỉnh thành:</strong> {form.city}</p>
-                    <p><strong>Quận / huyện:</strong> {form.district}</p>
-                    <p><strong>Phường / xã:</strong> {form.ward}</p>
-                    <p><strong>Số nhà, phố, tổ dân phố / thôn / đội:</strong> {form.house}</p>
-                    <p><strong>Điện thoại:</strong> {form.phone}</p>
-                    <p><strong>Email:</strong> {form.email}</p>
-                    <p><strong>Mô tả quốc gia / vùng lãnh thổ đã đến:</strong> {form.descrip}</p>
-                    <p><strong>Dấu hiệu xuất hiện:</strong> {form.token ? form.token.join(", ") : "Không có"}
-                    </p>
-                    <p><strong>Tiếp xúc với:</strong> {form.contact ? form.contact.join(", ") : "Không có"}</p>
+                <div className="mt-5 card shadow">
+                    <div className="card-body">
+                        <h2 className="card-title">Thông tin đã lưu</h2>
+                        <p><strong>Họ tên:</strong> {form.name}</p>
+                        <p><strong>Số hộ chiếu/CMND:</strong> {form.cmnd}</p>
+                        <p><strong>Năm sinh:</strong> {form.dob}</p>
+                        <p><strong>Giới tính:</strong> {form.gender}</p>
+                        <p><strong>Quốc tịch:</strong> {form.country}</p>
+                        <p><strong>Công ty làm việc:</strong> {form.company}</p>
+                        <p><strong>Bộ phận làm việc:</strong> {form.working}</p>
+                        <p><strong>Có thẻ bảo hiểm y tế:</strong> {form.insurance ? "Có" : "Không"}</p>
+                        <p><strong>Địa chỉ:</strong> {`${form.city}, ${form.district}, ${form.ward}, ${form.house}`}</p>
+                        <p><strong>Điện thoại:</strong> {form.phone}</p>
+                        <p><strong>Email:</strong> {form.email}</p>
+                        <p><strong>Mô tả quốc gia / vùng lãnh thổ:</strong> {form.descrip}</p>
+                        <p><strong>Dấu hiệu:</strong> {form.token ? form.token.join(", ") : "Không có"}</p>
+                        <p><strong>Tiếp xúc:</strong> {form.contact ? form.contact.join(", ") : "Không có"}</p>
+                    </div>
                 </div>
             )}
         </div>
     );
+
 }
